2ae91635181d5eff327b9346b59ec7ba45339e59
reisnermax <maximusreisner@gmail.com>
2019-02-19 16:19:56 -0500

    basic code

--- code/firmware.cpp
+++ code/firmware.cpp
@@ -7,20 +7,45 @@
 ros::NodeHandle nh;
 bauv_motor_control::motor_Values motor_values_msg;
 
-void motor_Cb(const bauv_motor_control::motor_Values& data) {
+Servo vfl;
+Servo vfr;
+Servo vbl;
+Servo vbr;
+Servo hfl;
+Servo hfr;
+Servo hbl;
+Servo hbr;
 
+void motor_Cb(const bauv_motor_control::motor_Values& data) {
+	vfl.writeMicroseconds(data.vfl);
+	vfr.writeMicroseconds(data.vfr);
+	vbl.writeMicroseconds(data.vbl);
+	vbr.writeMicroseconds(data.vbr);
+	hfl.writeMicroseconds(data.hfl);
+	hfr.writeMicroseconds(data.hfr);
+	hbl.writeMicroseconds(data.hbl);
+	hbr.writeMicroseconds(data.hbr);
 }
 
 ros::Subscriber<bauv_motor_control::motor_Values> sub("/thruster_values", &motor_Cb );
 
 void setup()
 {
-  nh.initNode();
-  nh.subscribe(sub);
+	nh.initNode();
+	nh.subscribe(sub);
+	
+	vfl.attach(2);
+	vfr.attach(3);
+	vbl.attach(4);
+	vbr.attach(5);
+	hfl.attach(6);
+	hfr.attach(7);
+	hbl.attach(8);
+	hbr.attach(9);
 }
 
 void loop()
 {
-  nh.spinOnce();
-  delay(1);
+	nh.spinOnce();
+	delay(1);
 }
