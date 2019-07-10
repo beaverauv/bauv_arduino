d6ec8aa24a8d267cbbd58f27aaf479f2681c8497
Max Reisner <maximusreisner@gmail.com>
2019-07-10 17:03:00 -0400

    work

--- code/firmware.cpp
+++ code/firmware.cpp
@@ -5,6 +5,13 @@
 #include <Arduino.h>
 #include <Servo.h>
 
+#define LIMITER 250
+
+#define ESC_MAX_F_VALUE 2000-LIMITER
+#define ESC_MIN_F_VALUE 1528
+#define ESC_MAX_R_VALUE 1000+LIMITER
+#define ESC_MIN_R_VALUE 1478
+
 ros::NodeHandle nh;
 motor_control::motor_Values motor_values_msg;
 
@@ -17,15 +24,27 @@ Servo shfr;
 Servo shbl;
 Servo shbr;
 
+int motor_map(int value) {
+	if (value > ESC_MIN_F_VALUE) {
+		return map(value, 0, 100, ESC_MIN_F_VALUE, ESC_MAX_F_VALUE);
+	}
+	else if (value < ESC_MIN_R_VALUE) {
+		return map(value, 0, -100, ESC_MIN_R_VALUE, ESC_MAX_R_VALUE);
+	}
+	else {
+		return 1500;
+	}
+}
+
 void motor_Cb(const motor_control::motor_Values& data) {
-	svfl.writeMicroseconds(data.vrf);
-	svfr.writeMicroseconds(data.vlf);
-	svbl.writeMicroseconds(data.vrb);
-	svbr.writeMicroseconds(data.vlb);
-	shfl.writeMicroseconds(data.hrf);
-	shfr.writeMicroseconds(data.hlf);
-	shbl.writeMicroseconds(data.hrb);
-	shbr.writeMicroseconds(data.hlb);
+	svfl.writeMicroseconds(motor_map(data.vrf));
+	svfr.writeMicroseconds(motor_map(data.vlf));
+	svbl.writeMicroseconds(motor_map(data.vrb));
+	svbr.writeMicroseconds(motor_map(data.vlb));
+	shfl.writeMicroseconds(motor_map(data.hrf));
+	shfr.writeMicroseconds(motor_map(data.hlf));
+	shbl.writeMicroseconds(motor_map(data.hrb));
+	shbr.writeMicroseconds(motor_map(data.hlb));
 }
 
 ros::Subscriber<motor_control::motor_Values> sub("/thruster_values", &motor_Cb );
