b2d72a3fa1880ff4fa47b86447563e1de8af500a
Maximus Reisner <maximusreisner@Maximuss-MacBook-Pro-4.local>
2019-07-12 17:02:02 -0400

    added untested hall code

--- code/firmware.cpp
+++ code/firmware.cpp
@@ -2,6 +2,7 @@
 #include <std_msgs/String.h>
 #include <motor_control/motor_Values.h>
 #include <sensor_msgs/BatteryState.h>
+#include <std_msgs/Bool.h>
 #include <Arduino.h>
 #include <Servo.h>
 
@@ -14,8 +15,14 @@
 
 #hall effect connected to A0
 
+bool flag = true;
+bool motor_enabled = true;
+
 ros::NodeHandle nh;
 motor_control::motor_Values motor_values_msg;
+std_msgs::Bool bool_msg;
+
+ros::Publisher motor_enabler("motor_enabled", &bool_msg);
 
 Servo svfl;
 Servo svfr;
@@ -39,14 +46,26 @@ int motor_map(int value) {
 }
 
 void motor_Cb(const motor_control::motor_Values& data) {
-	svfl.writeMicroseconds(motor_map(data.vrf));
-	svfr.writeMicroseconds(motor_map(data.vlf));
-	svbl.writeMicroseconds(motor_map(data.vrb));
-	svbr.writeMicroseconds(motor_map(data.vlb));
-	shfl.writeMicroseconds(motor_map(data.hrf));
-	shfr.writeMicroseconds(motor_map(data.hlf));
-	shbl.writeMicroseconds(motor_map(data.hrb));
-	shbr.writeMicroseconds(motor_map(data.hlb));
+	if (motor_enabled == true) {
+		svfl.writeMicroseconds(motor_map(data.vrf));
+		svfr.writeMicroseconds(motor_map(data.vlf));
+		svbl.writeMicroseconds(motor_map(data.vrb));
+		svbr.writeMicroseconds(motor_map(data.vlb));
+		shfl.writeMicroseconds(motor_map(data.hrf));
+		shfr.writeMicroseconds(motor_map(data.hlf));
+		shbl.writeMicroseconds(motor_map(data.hrb));
+		shbr.writeMicroseconds(motor_map(data.hlb));
+	}
+	else {
+                svfl.writeMicroseconds(1500);
+                svfr.writeMicroseconds(1500);
+                svbl.writeMicroseconds(1500);
+                svbr.writeMicroseconds(1500);
+                shfl.writeMicroseconds(1500);
+                shfr.writeMicroseconds(1500);
+                shbl.writeMicroseconds(1500);
+                shbr.writeMicroseconds(1500);
+	}
 }
 
 ros::Subscriber<motor_control::motor_Values> sub("/thruster_values", &motor_Cb );
@@ -55,7 +74,8 @@ void setup()
 {
 	nh.initNode();
 	nh.subscribe(sub);
-	
+	nh.advertise(motor_enabler);
+
 	svfl.attach(2);
 	svfr.attach(3);
 	svbl.attach(4);
@@ -78,6 +98,26 @@ void setup()
 
 void loop()
 {
+	int detectValue = abs(analogRead(A0)-515);
+	
+	if (detectValue > 50) {
+		if (flag == false) {
+			//Serial.println("Active");
+			flag = true;
+			motor_enabled = true
+		}
+	}
+	else {
+		if (flag == true) {
+			//Serial.println("Deactive");
+			flag = false;
+			motor_enabled = false
+		}
+	}
+
+	bool_msg.data = motor_enabled;
+	motor_enabler.publish(&bool_msg);
+
 	nh.spinOnce();
 	delay(1);
 }
